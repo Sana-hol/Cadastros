c51e8bcb853ca14e0841203cded0ad2ecfee4d4e
Saana <guilherme.ramone@gmail.com>
2023-04-24 17:39:06 -0300

    ajuste de novo

--- getlist.js
+++ getlist.js
@@ -22,30 +22,23 @@ function clearTableedit() {
   
   function EnviarCadastro(){
     const form = document.getElementById("cliente-form");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        const data = new FormData(form);
+    const data = new FormData(form);
         const xhr = new XMLHttpRequest();
         xhr.open("POST", "cliente.php");
         xhr.onload = () => {
         if (xhr.status === 200) {
             alert("Cliente cadastrado com sucesso!");
             form.reset()
-            clearTable()
-            GetList();
+            
         } else {
             alert("Ocorreu um erro ao cadastrar o cliente.");
         }
         };
         xhr.send(data);
-    });
+    clearTable();
+    };
 
-
-
-
-
-  };
+    
 function GetListEdit(){
   $(document).ready(function() {
       $.ajax({
